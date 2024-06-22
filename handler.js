0f0afac90f31224cbebf55bec9bb72c75995684b
MyBeets <keller67@berkeley.edu>
2024-06-22 15:14:08 -0700

    updating for array

--- handler.js
+++ handler.js
@@ -1,3 +1,6 @@
+var captionArr = null;
+var dictKeys = null;
+
 $( document ).ready(function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         console.time("side panel callback")
@@ -6,28 +9,57 @@ $( document ).ready(function() {
         var activeTab = tabs[0];
         var activeTabId = activeTab.id;
 
-        $.ajax({ 
-            type: "GET",
-            url: "https://perspective-plus-337f21f44542.herokuapp.com/captions", 
-            data: "id=" + activeTab.url,
-            dataType: "json",
+        if (captionArr == null) {
+            //get captions from server
+            $.ajax({ 
+                type: "GET",
+                url: "https://perspective-plus-337f21f44542.herokuapp.com/captions", 
+                data: "id=" + activeTab.url,
+                dataType: "json",
 
-            success: function(response_data){
-                console.log(response_data)
-                console.timeLog("side panel callback")
-                if (response_data.is_video == false) {
-                    document.getElementById("content").innerHTML = response_data.message;
-                } else {
-                    var captionDict = response_data.message；
-                    document.getElementById("content").innerHTML = JSON.stringify(captionDict);
-                    console.log(captionDict)
+                success: function(response_data){
+                    console.log(response_data);
+                    console.timeLog("side panel callback")
+                    if (response_data.is_video == false) {
+                        document.getElementById("content").innerHTML = response_data.message;
+                    } else {
+                        captionArr = response_data.message;
 
+                        document.getElementById("content").innerHTML = JSON.stringify(captionArr);
+                        console.log(captionArr);
+
+                        //listener to do on document update?
+                        timechecker(activeTabId);
+                        
+
+                    }
+                },
+                error : function(request,error)
+                {
+                    alert("request failed");
                 }
-            },
-            error : function(request,error)
-            {
-                alert("request failed");
-            }
-        })
+            })
+        } 
         });
 });
+
+async function timechecker(tabID){
+    res = await chrome.scripting.executeScript({
+      target: {tabId: tabID},
+      func: () => document.querySelector('.ytp-time-current')?.textContent,
+      //ytp-time-current
+    });
+    timestamp = convertToSeconds(res[0].result);
+    document.getElementById("timestamp").innerHTML = timestamp;
+}
+
+function convertToSeconds(timestamp) {
+    let arr = timestamp.split(":");
+    let seconds = 0;
+    for (let i = 0; i < arr.length; i++) {
+      let n = parseInt(arr[i]);
+      let exp = arr.length-1-i;
+      seconds += Math.pow(60, exp)*n; 
+    }
+    return seconds;
+}
